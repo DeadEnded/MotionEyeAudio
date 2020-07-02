893f89b8dc4958549b849d1805099dea8df839b4
DeadEnd <45110141+DeadEnded@users.noreply.github.com>
2020-07-02 14:27:58 -0400

    replace cameraID with camera_Name
    MotionEye does not set cameraID in the config file.
    Motion falls back on thread ID instead, which can lead to a mismatch.
    Changing to camera_name to prevent this error which leads to wrong audio devices.

--- motioneye-audio.sh
+++ motioneye-audio.sh
@@ -2,10 +2,14 @@
 
 # Set variables
 operation=$1
-camera_id=$2
+# Changing to Camera Name
+# camera_id=$2
+camera_name=%$
 file_path=$3
 motion_config_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-motion_camera_conf="${motion_config_dir}/camera-${camera_id}.conf"
+# Replacing with camera name due to cameraID/thread mismatch
+# motion_camera_conf="${motion_config_dir}/camera-${camera_id}.conf"
+motion_camera_conf="$( egrep -l \^camera_name.${camera_name} ${motion_config_dir}/*.conf)"
 netcam="$(if grep -q 'netcam_highres' ${motion_camera_conf};then echo 'netcam_highres'; else echo 'netcam_url'; fi)"
 extension="$(echo ${file_path} | sed 's/^/./' | rev | cut -d. -f1  | rev)"
 
@@ -16,13 +20,16 @@ case ${operation} in
         full_stream="$(echo ${stream} | sed -e "s/\/\//\/\/${credentials}@/")"
         ffmpeg -y -i "${full_stream}" -c:a aac ${file_path}.aac 2>&1 1>/dev/null &
         ffmpeg_pid=$!
-        echo ${ffmpeg_pid} > /tmp/motion-audio-ffmpeg-camera-${camera_id}
+        # echo ${ffmpeg_pid} > /tmp/motion-audio-ffmpeg-camera-${camera_id}
+        echo ${ffmpeg_pid} > /tmp/motion-audio-ffmpeg-camera-${camera_name}
         ;;
 
     stop)
         # Kill the ffmpeg audio recording for the clip
-        kill $(cat /tmp/motion-audio-ffmpeg-camera-${camera_id})
-        rm -rf $(cat /tmp/motion-audio-ffmpeg-camera-${camera_id})
+        # kill $(cat /tmp/motion-audio-ffmpeg-camera-${camera_id})
+        # rm -rf $(cat /tmp/motion-audio-ffmpeg-camera-${camera_id})
+        kill $(cat /tmp/motion-audio-ffmpeg-camera-${camera_name})
+        rm -rf $(cat /tmp/motion-audio-ffmpeg-camera-${camera_name})
 
         # Merge the video and audio to a single file, and replace the original video file
         ffmpeg -y -i ${file_path} -i ${file_path}.aac -c:v copy -c:a copy ${file_path}.temp.${extension};
@@ -33,6 +40,7 @@ case ${operation} in
         ;;
 
     *)
-        echo "Usage ./motioneye-audio.sh start <camera-id> <full-path-to-moviefile>"
+        # echo "Usage ./motioneye-audio.sh start <camera-id> <full-path-to-moviefile>"
+        echo "Usage ./motioneye-audio.sh start <camera-name> <full-path-to-moviefile>"
         exit 1
 esac
